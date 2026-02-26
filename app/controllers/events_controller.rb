ff94c395c4774ea6c7e6c0b130fc793232283dcb
Nicolas Goarant <>
2026-02-26 11:08:30 +0100

    Add admin events controller and views bis

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,55 +1,54 @@
-# app/controllers/events_controller.rb
-class EventsController < ApplicationController
-  def index
-    @events = Event.active.upcoming
+class Admin::EventsController < ApplicationController
+  layout false
 
-    respond_to do |format|
-      format.html { render layout: false }
-      format.json {
-        render json: @events.map { |event|
-          event.as_json.merge(
-            photo_url: event.photo_url  # Utilise la nouvelle méthode
-          )
-        }
-      }
-    end
+  def index
+    @events = Event.order(starts_at: :desc)
   end
 
   def show
-    @event = Event.friendly.find(params[:id])
-    @similar_events = Event.active
-                           .where(city: @event.city)
-                           .where.not(id: @event.id)
-                           .limit(3)
-    render layout: false
+    @event = Event.find(params[:id])
   end
 
   def new
     @event = Event.new
-    render layout: false
   end
 
   def create
     @event = Event.new(event_params)
-    @event.is_active = false
-
     if @event.save
-      redirect_to events_path, notice: "Merci ! Ton événement sera publié après validation."
+      redirect_to admin_events_path, notice: "Événement créé !"
     else
-      render :new, layout: false
+      render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      redirect_to admin_events_path, notice: "Événement mis à jour !"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to admin_events_path, notice: "Événement supprimé !"
   end
 
   private
 
   def event_params
     params.require(:event).permit(
-      :title, :description, :event_type, :level,
-      :venue_name, :address, :city, :postal_code,
-      :starts_at, :ends_at, :recurrence,
-      :price, :is_free, :has_lessons, :lessons_time,
-      :organizer_name, :organizer_email, :phone, :website, :facebook_url,
-      dance_styles: [], photos: []
+      :title, :description, :venue_name, :address, :city,
+      :starts_at, :price, :event_url, :is_active, :has_class,
+      :class_start_time, :class_end_time, :latitude, :longitude,
+      :level, :photo_url, dance_styles: []
     )
   end
 end
