1405bdd198d746c0563cad193f10ab66e4c6a7c8
Nicolas Goarant <>
2026-02-27 17:16:57 +0100

    rerefix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,14 +102,17 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  user_name: ENV['MAILTRAP_USERNAME'],
-  password: ENV['MAILTRAP_PASSWORD'],
-  address: 'sandbox.smtp.mailtrap.io',
-  port: 2525,
-  authentication: :plain
+
+  config.action_mailer.default_url_options = { host: 'www.salsaspot.fr', protocol: 'https' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['MAILTRAP_USERNAME'],
+    password: ENV['MAILTRAP_PASSWORD'],
+    address: 'sandbox.smtp.mailtrap.io',
+    port: 2525,
+    authentication: :plain
 }
-config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 end
