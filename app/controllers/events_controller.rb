474285749dc6a4a24baeeac17a856fcae1200dda
Nicolas Goarant <>
2026-02-27 16:50:32 +0100

    controllers

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,18 +5,18 @@ class EventsController < ApplicationController
     @events = Event.where(is_active: true).where("starts_at >= ?", Time.current).order(:starts_at)
     respond_to do |format|
       format.html
-        format.json do
-          render json: @events.map { |e|
-            default_style = (e.dance_styles & ['salsa', 'bachata', 'kizomba']).first || 'salsa'
-            fallback_photo = ActionController::Base.helpers.asset_path("#{default_style}.jpg")
+      format.json do
+        render json: @events.map { |e|
+          default_style = (e.dance_styles & ['salsa', 'bachata', 'kizomba']).first || 'salsa'
+          fallback_photo = ActionController::Base.helpers.asset_path("#{default_style}.jpg")
 
-            e.as_json.merge(
-              price_display: e.price_display,
-              photo_url: e.photo_url || fallback_photo,
-              date_display: I18n.l(e.starts_at, format: "%A %d %B %Y, %Hh")
-            )
-          }
-        end
+          e.as_json.merge(
+            price_display: e.price_display,
+            photo_url: e.photo_url || fallback_photo,
+            date_display: I18n.l(e.starts_at, format: "%A %d %B %Y, %Hh")
+          )
+        }
+      end
     end
   end
 
@@ -37,7 +37,9 @@ class EventsController < ApplicationController
     @event = Event.new(event_params)
     @event.is_active = false
     if @event.save
-      redirect_to merci_events_path
+      # Notification par email
+      EventMailer.new_event_notification(@event).deliver_later if defined?(EventMailer)
+      redirect_to merci_events_path, status: :see_other
     else
       render :new, status: :unprocessable_entity
     end
@@ -50,10 +52,13 @@ class EventsController < ApplicationController
 
   def event_params
     params.require(:event).permit(
-      :title, :description, :venue_name, :address, :city,
-      :starts_at, :price, :event_url, :has_class,
-      :class_start_time, :class_end_time, :latitude, :longitude,
-      :level, :photo_url, dance_styles: []
+      :title, :description, :venue_name, :address, :city, :postal_code,
+      :starts_at, :ends_at, :price, :is_free, :event_type,
+      :has_lessons, :lessons_time, :recurrence,
+      :level, :organizer_name, :phone, :organizer_email,
+      :website, :facebook_url,
+      :latitude, :longitude,
+      dance_styles: [], photos: []
     )
   end
 end
